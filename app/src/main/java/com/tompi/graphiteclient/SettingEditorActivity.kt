93004f45758904e3946f96596a966b688be65b28
Solymosi Tamas <>
2019-09-27 21:49:58 +0200

    editor fragment toolbar
    (cherry picked from commit c661d637472a42f9199f7def919f92b8c15af4ec)

--- app/src/main/java/com/tompi/graphiteclient/SettingEditorActivity.kt
+++ app/src/main/java/com/tompi/graphiteclient/SettingEditorActivity.kt
@@ -8,8 +8,18 @@ import kotlinx.android.synthetic.main.activity_setting_selector.*
 import org.slf4j.LoggerFactory
 
 import android.content.Context
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.tompi.graphiteclient.data.GraphiteSettings
+import android.app.Activity
+
+
 
 class SettingEditorActivity : AppCompatActivity(){
+
+//    private lateinit var toolbar:Toolbar
+    private lateinit var settingsId: String
 
     companion object {
         private val logger = LoggerFactory.getLogger("SettingEditorActivity")!!
@@ -24,12 +34,37 @@ class SettingEditorActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_editor)
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
+//
+//        toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
 
-        val id = intent.getStringExtra(SETTING_ID) ?: "noope"
-        logger.debug("id: $id")
+        settingsId = intent.getStringExtra(SETTING_ID) ?: "noope"
+        val graphiteSettings = GraphiteSettings.getSettingsByID(settingsId)
+        logger.debug("id: $settingsId - settings: ${graphiteSettings.toString()}")
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+        getMenuInflater().inflate(R.menu.menu_editor, menu)
+    }
 
-
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_cancel -> {
+            val returnIntent = Intent()
+            setResult(Activity.RESULT_CANCELED, returnIntent)
+            finish()
+            true
+        }
+        R.id.action_done -> {
+            val returnIntent = Intent()
+            returnIntent.putExtra("settingsId", settingsId)
+            setResult(Activity.RESULT_OK, returnIntent)
+            finish()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
--- app/src/main/java/com/tompi/graphiteclient/SettingEditorFragment.kt
+++ app/src/main/java/com/tompi/graphiteclient/SettingEditorFragment.kt
@@ -1,6 +1,8 @@
 package com.tompi.graphiteclient
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -11,6 +13,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.tompi.graphiteclient.data.GraphiteSettingItem
 import com.tompi.graphiteclient.data.GraphiteSettings
 
@@ -18,6 +21,8 @@ import kotlinx.android.synthetic.main.fragment_settingselector_listitem.view.*
 import kotlinx.android.synthetic.main.graphite_app_widget.view.*
 
 class SettingEditorFragment : Fragment() {
+    private lateinit var toolbar: Toolbar
+//    private lateinit var settingsId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,32 @@ class SettingEditorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_setting_editor, container, false)
+
+
+        toolbar = view.findViewById(R.id.toolbar)
+        toolbar.apply {
+            inflateMenu(R.menu.menu_editor)
+            setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.action_cancel -> {
+                        val returnIntent = Intent()
+                        requireActivity().setResult(Activity.RESULT_CANCELED, returnIntent)
+                        requireActivity().finish()
+                        true
+                    }
+                    R.id.action_done -> {
+                        val returnIntent = Intent()
+                        returnIntent.putExtra("settingsId", "asd")
+                        requireActivity().setResult(Activity.RESULT_OK, returnIntent)
+                        requireActivity().finish()
+                        true
+                    }
+                    else -> {
+                        super.onOptionsItemSelected(it)
+                    }
+                }
+            }
+        }
 
         return view
     }
